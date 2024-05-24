4ec112564a226c239de7dd659563f1fdfc8fd411
Cristinel Gingu <cristinel.gingu@cern.ch>
2024-05-24 09:58:50 -0500

    CG add template to be populated with code for cms_pix_28_fw_ip1.sv _ip2.sv and _ip3.sv

---
+++ cms_pix_28_test_firmware/src/cms_pix_28_fw_ip2.sv
@@ -0,0 +1,59 @@
+// ------------------------------------------------------------------------------------
+// Author       : Neha Kharwadkar      nehak@fnal.gov
+//              : Cristian Gingu       gingu@fnal.gov
+// Created      : 2024-05-22
+// ------------------------------------------------------------------------------------
+// Copyright (c) 2024 by FNAL This model is the confidential and
+// proprietary property of FNAL and the possession or use of this
+// file requires a written license from FNAL.
+// ------------------------------------------------------------------------------------
+// Revisions  :
+// Date        Author                 Description
+// 2024-05-24  Cristian  Gingu        Created template
+// ------------------------------------------------------------------------------------
+`ifndef __cms_pix_28_fw_ip2__
+`define __cms_pix_28_fw_ip2__
+
+`timescale 1 ns/ 1 ps
+
+module cms_pix_28_fw_ip2 (
+    input  logic        fw_clk,                            // FW clock              mapped to S_AXI_ACLK
+    input  logic        fw_rst_n,                          // FW reset, active low  mapped to S_AXI_ARESETN
+    // SW side signals from/to common_sw_to_fw_side
+    input  logic        fw_dev_id_enable,                  // up to 15 FW can be connected
+    input  logic        fw_op_code_w_reset,
+    input  logic        fw_op_code_w_cfg_static_0,
+    input  logic        fw_op_code_r_cfg_static_0,
+    input  logic        fw_op_code_w_cfg_array_0,
+    input  logic        fw_op_code_r_cfg_array_0,
+    input  logic        fw_op_code_w_cfg_array_1,
+    input  logic        fw_op_code_r_cfg_array_1,
+    input  logic        fw_op_code_r_data_array_0,
+    input  logic        fw_op_code_r_data_array_1,
+    input  logic        fw_op_code_r_status,
+    input  logic        fw_op_code_w_execute,
+    input  logic [23:0] sw_write24_0,                      // feed-through bytes 2, 1, 0 of sw_write32_0 from SW to FW
+    output logic [31:0] fw_read_data32,                    // 32-bit read_data   from FW to SW
+    output logic [31:0] fw_read_status32,                  // 32-bit read_status from FW to SW
+    // DUT side signals to/from common_fw_to_dut_side      // up to 15 FWs can be connected
+    // output signals from FW
+    output logic fw_config_clk,
+    output logic fw_reset_not,
+    output logic fw_config_in,
+    output logic fw_config_load,
+    output logic fw_bxclk_ana,
+    output logic fw_bxclk,
+    output logic fw_vin_test_trig_out,
+    output logic fw_scan_in,
+    output logic fw_scan_load,
+    // input signals to FW
+    input  logic fw_config_out,
+    input  logic fw_scan_out,
+    input  logic fw_dnn_output_0,
+    input  logic fw_dnn_output_1,
+    input  logic fw_dn_event_toggle
+  );
+
+endmodule
+
+`endif
