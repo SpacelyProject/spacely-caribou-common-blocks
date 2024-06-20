bf7f386d6499bffed49366951af91be763b4483f
Adam Quinn <aquinn@fnal.gov>
2024-06-20 12:55:08 -0500

    SPI Controller -- add option for ext spi clk

--- spi_controller_interface/src/spi_controller_interface_top.v
+++ spi_controller_interface/src/spi_controller_interface_top.v
@@ -13,75 +13,76 @@ module spi_controller_interface_top #(
 ) (
     
     // Ports to/from spi_controller to lpgtbFpga
-    input   wire   poci,
-    output  wire   pico,
-    output  wire   cs_b,
-    output  wire   spi_clk,
+    input wire 				      poci,
+    output wire 			      pico,
+    output wire 			      cs_b,
+    output wire 			      spi_clk,
+    input wire 				      ext_spi_clk,
 
     //////////////////////////////
     //    AXI BUS SIGNALS       //
     //////////////////////////////
 
     //	Global Clock Signal
-    input wire  S_AXI_ACLK,
+    input wire 				      S_AXI_ACLK,
     // Global Reset Signal. This Signal is Active LOW
-    input wire  S_AXI_ARESETN,
+    input wire 				      S_AXI_ARESETN,
     // Write address (issued by master, accepted by Slave)
-    input wire [C_S_AXI_ADDR_WIDTH-1 : 0] S_AXI_AWADDR,
+    input wire [C_S_AXI_ADDR_WIDTH-1 : 0]     S_AXI_AWADDR,
     // Write channel Protection type. This signal indicates the
     // privilege and security level of the transaction, and whether
     // the transaction is a data access or an instruction access.
-    input wire [2 : 0] S_AXI_AWPROT,
+    input wire [2 : 0] 			      S_AXI_AWPROT,
     // Write address valid. This signal indicates that the master signaling
     // valid write address and control information.
-    input wire  S_AXI_AWVALID,
+    input wire 				      S_AXI_AWVALID,
     // Write address ready. This signal indicates that the slave is ready
     // to accept an address and associated control signals.
-    output wire  S_AXI_AWREADY,
+    output wire 			      S_AXI_AWREADY,
     // Write data (issued by master, accepted by Slave)
-    input wire [C_S_AXI_DATA_WIDTH-1 : 0] S_AXI_WDATA,
+    input wire [C_S_AXI_DATA_WIDTH-1 : 0]     S_AXI_WDATA,
     // Write strobes. This signal indicates which byte lanes hold
     // valid data. There is one write strobe bit for each eight
     // bits of the write data bus.
     input wire [(C_S_AXI_DATA_WIDTH/8)-1 : 0] S_AXI_WSTRB,
     // Write valid. This signal indicates that valid write
     // data and strobes are available.
-    input wire  S_AXI_WVALID,
+    input wire 				      S_AXI_WVALID,
     // Write ready. This signal indicates that the slave
     // can accept the write data.
-    output wire  S_AXI_WREADY,
+    output wire 			      S_AXI_WREADY,
     // Write response. This signal indicates the status
     // of the write transaction.
-    output wire [1 : 0] S_AXI_BRESP,
+    output wire [1 : 0] 		      S_AXI_BRESP,
     // Write response valid. This signal indicates that the channel
     // is signaling a valid write response.
-    output wire  S_AXI_BVALID,
+    output wire 			      S_AXI_BVALID,
     // Response ready. This signal indicates that the master
     // can accept a write response.
-    input wire  S_AXI_BREADY,
+    input wire 				      S_AXI_BREADY,
     // Read address (issued by master, accepted by Slave)
-    input wire [C_S_AXI_ADDR_WIDTH-1 : 0] S_AXI_ARADDR,
+    input wire [C_S_AXI_ADDR_WIDTH-1 : 0]     S_AXI_ARADDR,
     // Protection type. This signal indicates the privilege
     // and security level of the transaction, and whether the
     // transaction is a data access or an instruction access.
-    input wire [2 : 0] S_AXI_ARPROT,
+    input wire [2 : 0] 			      S_AXI_ARPROT,
     // Read address valid. This signal indicates that the channel
     // is signaling valid read address and control information.
-    input wire  S_AXI_ARVALID,
+    input wire 				      S_AXI_ARVALID,
     // Read address ready. This signal indicates that the slave is
     // ready to accept an address and associated control signals.
-    output wire  S_AXI_ARREADY,
+    output wire 			      S_AXI_ARREADY,
     // Read data (issued by slave)
-    output wire [C_S_AXI_DATA_WIDTH-1 : 0] S_AXI_RDATA,
+    output wire [C_S_AXI_DATA_WIDTH-1 : 0]    S_AXI_RDATA,
     // Read response. This signal indicates the status of the
     // read transfer.
-    output wire [1 : 0] S_AXI_RRESP,
+    output wire [1 : 0] 		      S_AXI_RRESP,
     // Read valid. This signal indicates that the channel is
     // signaling the required read data.
-    output wire  S_AXI_RVALID,
+    output wire 			      S_AXI_RVALID,
     // Read ready. This signal indicates that the master can
     // accept the read data and response information.
-    input wire  S_AXI_RREADY
+    input wire 				      S_AXI_RREADY
 );
 
 // Instantiate spi_controller_interface
@@ -94,6 +95,7 @@ spi_controller_interface #(
     .pico(pico),
     .cs_b(cs_b),
     .spi_clk(spi_clk),
+    .ext_spi_clk(ext_spi_clk),
     .S_AXI_ACLK(S_AXI_ACLK),
     .S_AXI_ARESETN(S_AXI_ARESETN),
     .S_AXI_AWADDR(S_AXI_AWADDR),
