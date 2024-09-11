3313121f8b6bf5638be245db82c9784db5f25780
Cristinel Gingu <cristinel.gingu@cern.ch>
2024-09-11 18:10:12 -0500

    CG fix typo in posedge_counter/src/posedge_counter.sv

--- posedge_counter/src/posedge_counter.sv
+++ posedge_counter/src/posedge_counter.sv
@@ -21,56 +21,56 @@ module posedge_counter
 
 
    logic [(NUM_SIG-1):0][31:0] count_internal;
-   
+
 
    for (genvar i=0; i<NUM_SIG; i++) begin
       always_ff @(posedge input_signals[i], negedge axi_resetn) begin
-	 if (!axi_resetn) begin
-	    count_internal[i] <= 31'b0;
-	 end
-	 else begin
-	    count_internal[i] <= count_internal[i] + 1;
-	 end
+   if (!axi_resetn) begin
+      count_internal[i] <= 32'b0;
+   end
+   else begin
+      count_internal[i] <= count_internal[i] + 1;
+   end
       end
 end
 
 
    always_comb begin
       if (NUM_SIG > 0)
-	count_0 = count_internal[0];
+  count_0 = count_internal[0];
       else
-	count_0 = 0;
+  count_0 = 0;
 
       if (NUM_SIG > 1)
-	count_1 = count_internal[1];
+  count_1 = count_internal[1];
       else
-	count_1 = 0;
+  count_1 = 0;
 
       if (NUM_SIG > 2)
-	count_2 = count_internal[2];
+  count_2 = count_internal[2];
       else
-	count_2 = 0;
+  count_2 = 0;
       if (NUM_SIG > 3)
-	count_3 = count_internal[3];
+  count_3 = count_internal[3];
       else
-	count_3 = 0;
+  count_3 = 0;
       if (NUM_SIG > 4)
-	count_4 = count_internal[4];
+  count_4 = count_internal[4];
       else
-	count_4 = 0;
+  count_4 = 0;
       if (NUM_SIG > 5)
-	count_5 = count_internal[5];
+  count_5 = count_internal[5];
       else
-	count_5 = 0;
+  count_5 = 0;
       if (NUM_SIG > 6)
-	count_6 = count_internal[6];
+  count_6 = count_internal[6];
       else
-	count_6 = 0;
+  count_6 = 0;
       if (NUM_SIG > 7)
-	count_7 = count_internal[7];
+  count_7 = count_internal[7];
       else
-	count_7 = 0;
-      
-	end
+  count_7 = 0;
+
+  end
 
 endmodule // posedge_counter
